67e4ad52781dcaaeda1f965691c8e2c2e0eb6c9d
Liu Chun-Kai <bst0529@hotmail.com>
2022-03-03 11:47:08 +0800

    feat: 使用 Handlebars

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-//設定routes
+//引用 express-handlebars 模組
+const exphbs = require('express-handlebars')
+
+//告訴 Express 把樣板引擎交給 express-handlebars
+app.engine('hbs', exphbs.engine({extname: '.hbs',defaultLayout: 'main'}))
+//告訴 Express 要設定的 view engine 是 handlebars
+app.set('view engine', 'hbs')
+
+//設定 routes
 app.get('/', (req, res) => {
-    res.send('這裡是根路徑')
+    res.render('index')
 })
 
-//啟動並監聽Express Server
+//啟動並監聽 Express Server
 app.listen(port, () => {
     console.log(`Web is starting on http://localhost:${port}`)
 })
