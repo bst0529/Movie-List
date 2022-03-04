bc81ac70078ce673c11cf3541da187ca21c5a28d
Liu Chun-Kai <bst0529@hotmail.com>
2022-03-04 09:35:27 +0800

    feat: 讀取 JSON 檔案

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const port = 3000
 //引用 express-handlebars 模組
 const exphbs = require('express-handlebars')
 
+//載入 json 資料
+const movieList = require('./movies.json')
+
 //告訴 Express 把樣板引擎交給 express-handlebars
 app.engine('hbs', exphbs.engine({extname: '.hbs',defaultLayout: 'main'}))
 //告訴 Express 要設定的 view engine 是 handlebars
@@ -16,21 +19,8 @@ app.use(express.static('public'))
 
 //設定 routes
 app.get('/', (req, res) => {
-    //建立 movieList 儲存其內容
-    const movieList = [
-        {
-            id: 1,
-            title: 'Jurassic World: Fallen Kingdom',
-            image: 'https://movie-list.alphacamp.io/posters/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg'
-        },
-        {
-            id: 2,
-            title: 'Ant-Man and the Wasp',
-            image: 'https://movie-list.alphacamp.io/posters/rv1AWImgx386ULjcf62VYaW8zSt.jpg'
-        }
-    ]
     //將變數 movieList 傳遞至 partial template
-    res.render('index', { movies: movieList })
+    res.render('index', { movies: movieList.results })
 })
 
 //啟動並監聽 Express Server
